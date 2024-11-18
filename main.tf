9e2d523d6742d74ed2def59dd6dced58a48a5545
vijay1979-devops <vijay.devops.bha@gmail.com>
2024-11-18 06:32:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Project VPC"
+  }
+}
